3d1fc93f09dbc837adeaf7f6390f153a3e2b2142
lkj3085 <lkj3085@gmail.com>
2021-11-27 20:50:37 +0900

    ReactJs

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>How Works</p>
+        <p>NewsRoom</p>
+        <p>Investors</p>
+        <p>Plus</p>
+        <p>Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Community</h5>
+        <p>Accessibility</p>
+        <p>This Make JS</p>
+        <p>Clone</p>
+        <p>Referrals Accepted</p>
+        <p>김재호</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Host</h5>
+        <p>김재호</p>
+        <p>Presents</p>
+        <p>Zero to</p>
+        <p>Thanks Other Developer</p>
+        <p>More</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Support</h5>
+        <p>Help Center</p>
+        <p>Trust & Safefy</p>
+        <p>lkj3085@gmail.com</p>
+        <p>Please</p>
+        <p>Help</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
