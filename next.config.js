483d529275c7c937f60c4dec994722142f97ac29
lkj3085 <lkj3085@gmail.com>
2021-11-30 14:56:28 +0900

    Map build

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoibGtqMzA4NSIsImEiOiJja3dsbjJmcjkwM2VlMm5vMmswZjUwa3N2In0.0qiGEInvm1G59S5DS5gd5Q",
+  },
 };
